138b6b0c043674eafacceda9f86fba38bcdc992e
Marius Preikschat <m.preikschat@googlemail.com>
2021-06-22 14:03:35 +0200

    Initial Commit

---
+++ CircleRotation/CircleRotationApp.swift
@@ -0,0 +1,17 @@
+//
+//  CircleRotationApp.swift
+//  CircleRotation
+//
+//  Created by Marius Preikschat on 22.06.21.
+//
+
+import SwiftUI
+
+@main
+struct CircleRotationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
