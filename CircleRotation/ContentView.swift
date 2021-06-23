8c9af29e69586cb6e3395178a3131b345b57f21d
Marius Preikschat <m.preikschat@googlemail.com>
2021-06-23 17:17:27 +0200

    minor tweaks

--- CircleRotation/ContentView.swift
+++ CircleRotation/ContentView.swift
@@ -8,6 +8,32 @@
 import SwiftUI
 
 struct ContentView: View {
+    var body: some View {
+        VStack(spacing: 50) {
+            CircleFilled()
+            CircleStroke()
+        }
+    }
+}
+
+struct CircleFilled: View {
+    @State private var isLoading = false
+    
+    var body: some View {
+        Circle()
+            .fill(
+                AngularGradient(gradient: Gradient(colors: [.red, .yellow, .green, .blue, .purple, .red]), center: .center, startAngle: .zero, endAngle: .degrees(360))
+            )
+            .frame(width: 200, height: 200)
+            .rotationEffect(Angle(degrees: isLoading ? 360 : 0))
+            .animation(Animation.default.repeatForever(autoreverses: false))
+            .onAppear() {
+                self.isLoading = true
+            }
+    }
+}
+
+struct CircleStroke: View {
     @State private var isLoading = false
     
     var body: some View {
@@ -18,7 +44,6 @@ struct ContentView: View {
             )
             .frame(width: 200, height: 200)
             .rotationEffect(Angle(degrees: isLoading ? 360 : 0))
-            .scaleEffect(isLoading ? 1 : 0.9)
             .animation(Animation.default.repeatForever(autoreverses: false))
             .onAppear() {
                 self.isLoading = true
